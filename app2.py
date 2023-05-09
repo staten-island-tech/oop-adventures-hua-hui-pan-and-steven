162eecc6b9731a259b1d108738dd27f35f295a80
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-09 08:37:52 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,9 +8,10 @@ class bronze_sword(weapon):
         super().__init__(equip_correct_class)
 
 class silver_sword(weapon):
-
+    def __init__(self, equip_correct_class, silver_sword):
+        super().__init__(equip_correct_class)
 
 class gold_sword(weapon):
-
+    
 
 class diamond_sword(weapon):
