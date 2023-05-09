792db12da52754abfb3ea376c8fbd5f78b239a30
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-09 08:36:04 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,16 @@
+import uuid
+class weapon:
+    def __init__(self,equip_correct_class):
+        self.equip_correct_class = equip_correct_class
 
+class bronze_sword(weapon):
+    def __init__(self, equip_correct_class, bronze_sword):
+        super().__init__(equip_correct_class)
+
+class silver_sword(weapon):
+
+
+class gold_sword(weapon):
+
+
+class diamond_sword(weapon):
