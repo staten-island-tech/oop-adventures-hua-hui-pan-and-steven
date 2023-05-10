f5f3f4e6faa859f2ad339363a675cf1f0619e783
HuaHuiPan <mearenoobl0515@gmail.com>
2023-05-10 08:37:48 -0400

    npc

--- npc.py
+++ npc.py
@@ -4,4 +4,14 @@ class mobs:
         self.name=name
         self.attack=attack
 
-class 
+class shadow_soldiers(mobs):
+    def __init__(self,name,attack,shadowslash):
+        super().__init__(name,attack)
+        self.shadowslash= shadowslash
+    def __str__(self):
+            return f"{self.name},{self.attack},{self.shadowslash}"
+class shadow_golem(mobs):
+     def __init__(self,name,attack,)    
+
+
+    
