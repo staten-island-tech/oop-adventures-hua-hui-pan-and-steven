35e87c90d7745e172218bcf1cc9aa81e78360f9b
HuaHuiPan <mearenoobl0515@gmail.com>
2023-05-11 08:38:49 -0400

    npc2

--- npc.py
+++ npc.py
@@ -11,7 +11,30 @@ class shadow_soldiers(mobs):
     def __str__(self):
             return f"{self.name},{self.attack},{self.shadowslash}"
 class shadow_golem(mobs):
-     def __init__(self,name,attack,)    
+    def __init__(self,name,attack,shadowslash,shadowbomb):
+          super().__init__(name,attack,shadowslash)
+          self.shadowbomb=shadowbomb
+    def __str__(self):
+          return f"{self.name},{self.attack},{self.shadowslash},{self.shadowbomb}"
+class shadow_giants(mobs):
+    def __init__(self,name,attack,shadowslash,shadowbomb,shadowslam):
+          super().__init__(self,name,attack,shadowslash,shadowbomb)
+          self.shadowslam=shadowslam
+    def __str__(self):
+          return f"{self.name},{self.attack},{self.shadowslash}{self.shadowbomb},{self.shadowslam}"
+    
+shadowslash_attack= 10
+shadowbomb_attack= 25
+shadowslam_attack= 40
+
+Hp=50
+Hp=100
+Hp=150
+
+     
+
+     
+              
 
 
     
