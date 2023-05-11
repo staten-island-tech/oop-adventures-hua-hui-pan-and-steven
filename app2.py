7a9246583c73abe21325703c009974a1125ddd80
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-11 08:39:32 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,24 +5,24 @@ class sword:
         self.swordsmaster = swordsmaster
 
 class silver_sword(sword):
-    def __init__(self, swordsmaster, silver_sword):
+    def __init__(self, swordsmaster, silver_sword, silver_slash):
         super().__init__(swordsmaster)
         self.silver_sword = silver_sword
     def __str__(self):
         return f"{self.swordsmaster}, {self.silver_sword}"
-class gold_sword(sword):
-    def __init__(self, swordsmaster, gold_sword):
+class gold_sword(sword): 
+    def __init__(self, swordsmaster, gold_sword, gold_slash):
         super().__init__(swordsmaster)
         self.gold_sword = gold_sword
     def __str__(self):
         return f"{self.swordsmaster}, {self.gold_sword}"
 class diamond_sword(sword):
-    def __init__(self, swordsmaster, diamond_sword):
+    def __init__(self, swordsmaster, diamond_sword, diamond_slash):
         super().__init__(swordsmaster)
         self.diamond_sword = diamond_sword
     def __str__(self):
-        return f"{self.swordsmaster}, {self.diamond_sword}"
-    
+        return f"{self.swordsmaster}, {self.diamond_sword}, {self.diamond_slash}"
+
 
 
 
