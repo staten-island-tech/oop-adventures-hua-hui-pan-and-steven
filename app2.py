7d0ebd9f43a3d395dcd0c574144997afb1c3ed58
HuaHuiPan <mearenoobl0515@gmail.com>
2023-05-12 08:36:21 -0400

    34uhueifiwdfiudkewcbw

--- app2.py
+++ app2.py
@@ -1,1 +1,72 @@
+import uuid
+class user:
+   def __init__(self,name):
+      self.name=name
+
+class swordsmaster(user):
+   def __init__(self,name,sword):
+      super().__init__(name)
+      self.sword= sword
+   def __str__(self):
+         return f"{self.name}, {self.sword}"
+class mage(user):
+   def __init__(self,name,staff):
+      super().__init__(name)
+      self.staff=staff
+   def __str__(self):
+      return f"{self.name}, {self.staff}"
+class beserker(user):
+   def __init__(self,name,axe):
+      super().__init__(name)
+      self.axe=axe
+   def __str__(self):
+      return f"{self.name}, {self.axe}"
+   
+
+class mobs:
+    def __init__(self,name,attack):
+        self.name=name
+        self.attack=attack
+
+class shadow_soldiers(mobs):
+    def __init__(self,name,attack,shadowslash):
+        super().__init__(name,attack)
+        self.shadowslash= shadowslash
+    def __str__(self):
+            return f"{self.name},{self.attack},{self.shadowslash}"
+class shadow_golem(mobs):
+    def __init__(self,name,attack,shadowslash,shadowbomb):
+          super().__init__(name,attack,shadowslash)
+          self.shadowbomb=shadowbomb
+          self.shadowslash=shadowslash
+    def __str__(self):
+          return f"{self.name},{self.attack},{self.shadowslash},{self.shadowbomb}"
+class shadow_giants(mobs):
+    def __init__(self,name,attack,shadowslash,shadowbomb,shadowslam):
+          super().__init__(self,name,attack,shadowslash,shadowbomb)
+          self.shadowslam=shadowslam
+          self.shadowbomb=shadowbomb
+          self.shadowslash=shadowslash
+    def __str__(self):
+          return f"{self.name},{self.attack},{self.shadowslash}{self.shadowbomb},{self.shadowslam}"
+    
+shadowslash_attack= 10
+shadowbomb_attack= 25
+shadowslam_attack= 40
+
+Hp=50
+Hp=100
+Hp=150
+
+swordsmasters=[]
+mages=[]
+beserkers=[]
+
+def create_new_swordsmaster(name,sword):
+    id = str(uuid.uuid4())
+    new_swordsmaster= swordsmaster(name,sword)
+    swordsmasters.append(new_swordsmaster)
+    for swordsmater in swordsmasters:
+        print(swordsmaster)
+    
 
