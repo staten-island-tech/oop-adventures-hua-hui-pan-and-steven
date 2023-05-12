20580b58d013e7697a369e217f5f8f655ac1045d
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-12 08:25:10 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -38,24 +38,31 @@ class staff:
         self.mage = mage
 
 class silver_staff(staff):
-    def __init__(self, mage, silver_staff):
-        super().__init__(mage)
+    def __init__(self, mage, silver_staff, attack, silver_shot):
+        super().__init__(mage, attack, silver_shot)
         self.silver_staff = silver_staff
+        self.silver_shot = silver_shot
     def __str__(self):
-        return f"{self.mage}, {self.silver_staff}"
+        return f"{self.mage}, {self.silver_staff}, {self.silver_shot}"
 class gold_staff(staff):
-    def __init__(self, mage, gold_staff):
-        super().__init__(mage)
+    def __init__(self, mage, gold_staff, attack, gold_shot):
+        super().__init__(mage, attack, gold_shot)
         self.gold_staff = gold_staff
+        self.gold_shot = gold_shot
     def __str__(self):
-        return f"{self.mage}, {self.gold_staff}"
+        return f"{self.mage}, {self.gold_staff}, {self.gold_shot}"
 class diamond_staff(staff):
-    def __init__(self, mage, diamond_staff):
-        super().__init__(mage)
+    def __init__(self, mage, diamond_staff, attack, diamond_shot):
+        super().__init__(mage, attack, diamond_shot)
         self.diamond_staff = diamond_staff
+        self.diamond_shot = diamond_shot
     def __str__(self):
-        return f"{self.mage}, {self.diamond_staff}"
+        return f"{self.mage}, {self.diamond_staff}, {self.diamond_shot}"
     
+silver_shot = 5
+gold_shot = 10
+diamond_shot = 20
+
 
 
 
@@ -64,20 +71,27 @@ class axe:
         self.beserker = beserker
 
 class silver_axe(axe):
-    def __init__(self, beserker, silver_axe):
-        super().__init__(beserker)
+    def __init__(self, beserker, silver_axe, attack, silver_smash):
+        super().__init__(beserker, attack, silver_smash)
         self.silver_axe = silver_axe
+        self.silver_smash = silver_smash
     def __str__(self):
-        return f"{self.beserker}, {self.silver_axe}"
+        return f"{self.beserker}, {self.silver_axe}, {self.silver_smash}"
 class gold_axe(axe):
-    def __init__(self, beserker, gold_axe):
-        super().__init__(beserker)
+    def __init__(self, beserker, gold_axe, attack, gold_smash):
+        super().__init__(beserker, attack, gold_smash)
         self.gold_axe = gold_axe
+        self.gold_smash = gold_smash
     def __str__(self):
-        return f"{self.beserker}, {self.gold_axe}"
+        return f"{self.beserker}, {self.gold_axe}, {self.gold_smash}"
 class diamond_axe(axe):
-    def __init__(self, beserker, diamond_axe):
-        super().__init__(beserker)
+    def __init__(self, beserker, diamond_axe, attack, diamond_smash):
+        super().__init__(beserker, attack, diamond_smash)
         self.diamond_axe = diamond_axe
+        self.diamond_smash = diamond_smash
     def __str__(self):
-        return f"{self.beserker}, {self.diamond_axe}"
+        return f"{self.beserker}, {self.diamond_axe}, {self.diamond_smash}"
+    
+silver_smash = 20
+gold_smash = 30
+diamond_smash = 50
