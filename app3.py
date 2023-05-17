f673efcb5395a1c5a6bf78bf46534e90d250a33c
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-17 07:58:57 -0400

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,3 +1,10 @@
 class area:
     def __init__(self,name):
         self.name = name
+
+class shadow_castle(area):
+    def __init__(self, name, location):
+        super().__init__(name)
+        self.location = location
+    def __str__(self):
+        return f"{self.name}, {self.location}"
