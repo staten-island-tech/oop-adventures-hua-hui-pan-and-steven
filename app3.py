8c3e9e983d621cc4e1b9013abe048f169497ce20
HuaHuiPan <mearenoobl0515@gmail.com>
2023-05-22 08:39:07 -0400

    kmno

---
+++ app3.py
@@ -0,0 +1,19 @@
+class area:
+    def __init__(self,name):
+        self.name = name
+
+class shadow_castle(area):
+    def __init__(self, name, location):
+        super().__init__(name)
+        self.location = location
+    def __str__(self):
+        return f"{self.name}, {self.location}"
+    
+class shadow_castle_second_floor(area):
+    def __init__(self,name,location):
+        super().__init__(name)
+        self.location=location
+    def __str__(self):
+        return f"{self.name},{self.location}"
+    
+
