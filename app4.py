eb53ff123e079cbe7c57d03ef0e236858c8e4ccd
SteveLi2023 <stevenl284@nycstudents.net>
2023-05-25 08:22:01 -0400

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,5 +1,26 @@
 import math
 
+class area:
+    def __init__(self,name):
+        self.name = name
+
+class shadow_castle_first_floor(area):
+    def __init__(self, name, location):
+        super().__init__(name)
+        self.location = location
+    def __str__(self):
+        return f"{self.name}, {self.location}"
+    
+class shadow_castle_second_floor(area):
+    def __init__(self,name,location):
+        super().__init__(name)
+        self.location = location
+    def __str__(self):
+        return f"{self.name},{self.location}"
+    
+
+
+
 
 swordsmaster =[]
 mage = []
